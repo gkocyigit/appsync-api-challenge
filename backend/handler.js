375d6cdae0cd1ab62d3549036e897c256e8350f2
Gokhan <gokhanhkocyigit@gmail.com>
2021-07-10 17:35:17 +0300

    bugfix on responses

--- backend/handler.js
+++ backend/handler.js
@@ -6,48 +6,62 @@ AWS.config.update({
 })
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
-module.exports.getStudents = async (event) => {
-   console.log(event);
+module.exports.getStudents = async (event) =>  {
    var params={
       TableName:"Student"
    }
 
-   await dynamo.scan(params,function(err,data){
-      console.log(JSON.stringify(data.Items))
-      if(err){
-         return {"error": "There is an error while retrieving students"}
-      }else{
-         result=data.Items.map((x)=>new {"name":x.name,"score":x.score})
-         console.log(result);
-         return result;
-      }
+   return new Promise((resolve,reject)=>{
+      dynamo.scan(params,function(err,data){
+         if(err){
+            console.log(err)
+            reject({"error": "There is an error while retrieving students"})
+         }
+         resolve(data.Items);
+      })
    })
 };
 
 module.exports.getStudent = async (event) => {
-   console.log(JSON.stringify(event));
-   return { name: 'Gokhan', score: '100' };
+
+   var params={
+      TableName:"Student",
+      Key:{
+         "name":event["arguments"]["name"]
+      }
+   }
+
+   return new Promise((resolve,reject)=>{
+      dynamo.get(params,function(err,data){
+         if(err){
+            console.log(err)
+            reject({"error": "There is an error while retrieving students"})
+         }
+         resolve(data.Items);
+      })
+   })
 };
 
-module.exports.addStudent = (event) => {
-   console.log(JSON.stringify(event));
+module.exports.addStudent = async (event) => {
    var student={name:event["arguments"]["name"]}
    if ("score" in event["arguments"]){
       student["score"]=event["arguments"]["score"]
    }
-   console.log(student)
    var params={
       TableName:"Student",
       Item:student,
    }
 
-   dynamo.put(params).promise()
-   .then(()=>{
-      return student
+   return new Promise((resolve,reject)=>{
+      dynamo.put(params).promise()
+      .then(()=>{
+         resolve(student)
+      })
+      .catch((err)=>{
+         console.log(err);
+         reject({"error": "There is an error while adding new student"})
+      })
    })
-   .catch((err)=>{
-      console.log(err);
-      return {"error": "There is an error while adding new student"}
-   })
+   
 };
 
